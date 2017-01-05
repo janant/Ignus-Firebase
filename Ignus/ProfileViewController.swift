1878f0b393ba599dc42ec9346bf326d15acc89d9
Anant Jain <anantjain98@me.com>
2017-01-04 22:08:13 -0800

    Add Profile View data
    Link profile views to the view controller
    Add profile data to the profile views, including name, username, and
    photos

--- Ignus/ProfileViewController.swift
+++ Ignus/ProfileViewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 class ProfileViewController: UIViewController {
+    
+    // Profile header views
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var coverImageView: UIImageView!
     
     @IBOutlet weak var selectUserLabel: UILabel!
     @IBOutlet weak var profileView: UIView!
@@ -27,6 +34,45 @@ class ProfileViewController: UIViewController {
             return
         }
         selectUserLabel.isHidden = true
+        
+        // Gets user information
+        guard
+            let firstName = profileInfo["firstName"],
+            let lastName = profileInfo["lastName"],
+            let username = profileInfo["username"]
+        else {
+            return
+        }
+        
+        // Gets profile and cover photo references from Firebase
+        let storageRef = FIRStorage.storage().reference()
+        let profileRef = storageRef.child("User_Pictures/\(username)/profile.jpg")
+        let coverRef = storageRef.child("User_Pictures/\(username)/cover.jpg")
+        
+        // Loads profile picture
+        profileRef.data(withMaxSize: INT64_MAX, completion: { (data, error) in
+            if error == nil && data != nil {
+                UIView.transition(with: self.profileImageView, duration: 0.2, options: .transitionCrossDissolve, animations: {
+                    self.profileImageView.image = UIImage(data: data!)
+                }, completion: nil)
+            }
+        })
+        // Loads cover picture
+        coverRef.data(withMaxSize: INT64_MAX, completion: { (data, error) in
+            if error == nil && data != nil {
+                UIView.transition(with: self.coverImageView, duration: 0.2, options: .transitionCrossDissolve, animations: {
+                    self.coverImageView.image = UIImage(data: data!)
+                }, completion: nil)
+            }
+        })
+        
+        // Fades in new data
+        UIView.transition(with: self.nameLabel, duration: 0.2, options: .transitionCrossDissolve, animations: {
+            self.nameLabel.text = "\(firstName) \(lastName)"
+        }, completion: nil)
+        UIView.transition(with: self.usernameLabel, duration: 0.2, options: .transitionCrossDissolve, animations: {
+            self.usernameLabel.text = username
+        }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {

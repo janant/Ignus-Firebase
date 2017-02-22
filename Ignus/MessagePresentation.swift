77b256be85facda59b021db163eba2581a0e0f0e
Anant Jain <anantjain98@me.com>
2017-02-21 17:22:29 -0800

    Create code structure for message transition animation and presentation

--- Ignus/MessagePresentation.swift
+++ Ignus/MessagePresentation.swift
@@ -10,4 +10,57 @@ import UIKit
 
 class MessagePresentation: UIPresentationController {
 
+    let shadowView = UIView()
+    
+    override var frameOfPresentedViewInContainerView: CGRect {
+        return CGRect(x: 20, y: 20, width: min(Int(presentingViewController.view.frame.width) - 40, 400), height: 244)
+    }
+    
+    override func presentationTransitionWillBegin() {
+        // Sets up shadow view and adds to container view
+        shadowView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.4)
+        shadowView.frame = presentingViewController.view.frame
+        shadowView.alpha = 0.0
+        if let container = self.containerView {
+            container.addSubview(shadowView)
+            container.addSubview(presentedViewController.view)
+        }
+        if let transitionCoordinator = presentingViewController.transitionCoordinator {
+            transitionCoordinator.animate(alongsideTransition: { (context) in
+                self.shadowView.alpha = 1.0
+            }, completion: nil)
+        }
+    }
+    
+    override func presentationTransitionDidEnd(_ completed: Bool) {
+        if !completed {
+            shadowView.removeFromSuperview()
+        }
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        if presentingViewController.traitCollection.horizontalSizeClass == .regular {
+            if let transitionCoordinator = presentingViewController.transitionCoordinator {
+                transitionCoordinator.animate(alongsideTransition: { (context) in
+                    self.shadowView.alpha = 0.0
+                }, completion: nil)
+            }
+        }
+    }
+    
+    override func dismissalTransitionDidEnd(_ completed: Bool) {
+        if completed {
+            shadowView.removeFromSuperview()
+        }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        coordinator.animate(alongsideTransition: { (context) in
+            self.presentedViewController.view.frame = self.frameOfPresentedViewInContainerView
+            self.shadowView.frame = self.presentingViewController.view.frame
+        }, completion: nil)
+    }
+
 }
--- Ignus/MessageTransition.swift
+++ Ignus/MessageTransition.swift
@@ -9,5 +9,115 @@
 import UIKit
 
 class MessageTransition: NSObject {
+    
+    var presenting: Bool
+    var isViewingMessage: Bool
+    var sentMessage: Bool
+    
+    init(presenting: Bool, isViewingMessage: Bool = false, sentMessage: Bool = false) {
+        self.presenting = presenting
+        self.isViewingMessage = isViewingMessage
+        self.sentMessage = sentMessage
+    }
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.15
+    }
+    
+    func composePresentAnimation(using transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+    
+    func composeDismissAnimation(using transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+    
+    func replyDismissAnimation(using transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+    
+    func viewMessagePresentAnimation(using transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+    
+    func viewMessageDismissAnimation(using transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        if presenting {
+            if isViewingMessage {
+                viewMessagePresentAnimation(using: transitionContext)
+            }
+            else {
+                composePresentAnimation(using: transitionContext)
+            }
+        }
+        else {
+            if sentMessage {
+                replyDismissAnimation(using: transitionContext)
+            }
+            else if isViewingMessage {
+                viewMessageDismissAnimation(using: transitionContext)
+            }
+            else {
+                composeDismissAnimation(using: transitionContext)
+            }
+        }
+//        if presenting {
+//            let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
+//            let toView = transitionContext.view(forKey: UITransitionContextViewKey.to)!
+//            let containerView = transitionContext.containerView
+//            
+//            let darkView = UIView()
+//            darkView.frame = UIScreen.main.bounds
+//            darkView.backgroundColor = UIColor.black
+//            darkView.alpha = 0.0
+//            
+//            toView.frame = CGRect(x: 20, y: 40, width: UIScreen.main.bounds.size.width - 40, height: 244)
+//            toView.alpha = 0.0
+//            toView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+//            toView.layer.cornerRadius = 10
+//            toView.layer.masksToBounds = true
+//            
+//            let toVC = (transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to) as! UINavigationController).topViewController as! ComposeMessageViewController
+//            toVC.shadowView = darkView
+//            
+//            containerView.addSubview(darkView)
+//            containerView.addSubview(toView)
+//            
+//            UIView.animate(withDuration: self.transitionDuration(using: transitionContext), delay: 0, options: .curveEaseOut, animations: { () -> Void in
+//                darkView.alpha = 0.7
+//                toView.alpha = 1.0
+//                toView.transform = CGAffineTransform.identity
+//            }, completion: { (completed) -> Void in
+//                transitionContext.completeTransition(true)
+//            })
+//        }
+//        else {
+//            let fromView = transitionContext.view(forKey: UITransitionContextViewKey.from)!
+//            let fromVC = (transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from) as! UINavigationController).topViewController as! ComposeMessageViewController
+//            let darkView = fromVC.shadowView!
+//            
+//            if self.messageSent {
+//                UIView.animate(withDuration: self.transitionDuration(using: transitionContext) * 1.5, delay: 0, options: .curveEaseOut, animations: { () -> Void in
+//                    darkView.alpha = 0.0
+//                    fromView.transform = CGAffineTransform(translationX: 0, y: -288)
+//                }, completion: { (completed) -> Void in
+//                    transitionContext.completeTransition(true)
+//                })
+//                
+//            }
+//            else {
+//                UIView.animate(withDuration: self.transitionDuration(using: transitionContext), delay: 0, options: .curveEaseOut, animations: { () -> Void in
+//                    darkView.alpha = 0.0
+//                    fromView.alpha = 0.0
+//                    fromView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+//                }, completion: { (completed) -> Void in
+//                    transitionContext.completeTransition(true)
+//                })
+//            }
+//        }
+    }
 
 }

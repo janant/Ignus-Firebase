b5adb6e3359ffc96c3b7cb0a316475f94041784e
Anant Jain <anantjain98@me.com>
2017-03-16 22:40:31 -0700

    Create files for payment view controllers, create files for request payment transition/presentation

---
+++ Ignus/RequestPaymentPresentation.swift
@@ -0,0 +1,13 @@
+//
+//  RequestPaymentPresentation.swift
+//  Ignus
+//
+//  Created by Anant Jain on 3/16/17.
+//  Copyright © 2017 Anant Jain. All rights reserved.
+//
+
+import UIKit
+
+class RequestPaymentPresentation: UIPresentationController {
+
+}
---
+++ Ignus/RequestPaymentTransition.swift
@@ -0,0 +1,13 @@
+//
+//  RequestPaymentTransition.swift
+//  Ignus
+//
+//  Created by Anant Jain on 3/16/17.
+//  Copyright © 2017 Anant Jain. All rights reserved.
+//
+
+import UIKit
+
+class RequestPaymentTransition: NSObject {
+
+}

d0d75ad1fc28f99b3cde3f2b0a2261c071a84ac9
Anant Jain <anantjain98@me.com>
2017-03-18 20:37:27 -0700

    Create request payment transition and presentation

--- Ignus/RequestPaymentPresentation.swift
+++ Ignus/RequestPaymentPresentation.swift
@@ -9,5 +9,72 @@
 import UIKit
 
 class RequestPaymentPresentation: UIPresentationController {
-
+    
+    let shadowView = UIView()
+    
+    override var frameOfPresentedViewInContainerView: CGRect {
+        let presentingFrame = presentingViewController.view.frame
+        let intendedWidth: CGFloat = 500 // Want to make the view at most 500 pixels wide
+        let intendedHeight: CGFloat = 500 // Want to make the view at most 500 pixels tall
+        
+        let maxPossibleWidth: CGFloat = presentingFrame.width - 40 // View width, 20px side margins
+        let maxPossibleHeight: CGFloat = presentingFrame.height - 40 // View height, 20px top/bottom
+        
+        let actualWidth = min(intendedWidth, maxPossibleWidth)
+        let actualHeight = min(intendedHeight, maxPossibleHeight)
+        
+        return CGRect(x: presentingFrame.midX - (actualWidth / 2), y: presentingFrame.midY - (actualHeight / 2), width: actualWidth, height: actualHeight)
+    }
+    
+    override func presentationTransitionWillBegin() {
+        // Sets up shadow view and adds to container view
+        shadowView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.4)
+        shadowView.frame = presentingViewController.view.frame
+        shadowView.alpha = 0.0
+        
+        // Adds corner radius to message view controller
+        presentedViewController.view.layer.cornerRadius = 10
+        presentedViewController.view.layer.masksToBounds = true
+        
+        if let container = self.containerView {
+            container.addSubview(shadowView)
+            container.addSubview(presentedViewController.view)
+        }
+        
+        if let transitionCoordinator = presentingViewController.transitionCoordinator {
+            transitionCoordinator.animate(alongsideTransition: { (context) in
+                self.shadowView.alpha = 1.0
+            }, completion: nil)
+        }
+    }
+    
+    override func presentationTransitionDidEnd(_ completed: Bool) {
+        if !completed {
+            shadowView.removeFromSuperview()
+        }
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        if let transitionCoordinator = presentingViewController.transitionCoordinator {
+            transitionCoordinator.animate(alongsideTransition: { (context) in
+                self.shadowView.alpha = 0.0
+            }, completion: nil)
+        }
+    }
+    
+    override func dismissalTransitionDidEnd(_ completed: Bool) {
+        if completed {
+            shadowView.removeFromSuperview()
+        }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        coordinator.animate(alongsideTransition: { (context) in
+            self.presentedViewController.view.frame = self.frameOfPresentedViewInContainerView
+            self.shadowView.frame = self.presentingViewController.view.frame
+        }, completion: nil)
+    }
+    
 }
--- Ignus/RequestPaymentTransition.swift
+++ Ignus/RequestPaymentTransition.swift
@@ -8,6 +8,88 @@
 
 import UIKit
 
-class RequestPaymentTransition: NSObject {
-
+class RequestPaymentTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    var presenting: Bool
+    var sentMessage: Bool
+    
+    init(presenting: Bool, sentMessage: Bool = false) {
+        self.presenting = presenting
+        self.sentMessage = sentMessage
+    }
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.25
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        if presenting {
+            animatePresentation(using: transitionContext)
+        }
+        else {
+            if sentMessage {
+                animateRequestSent(using: transitionContext)
+            }
+            else {
+                animateCancel(using: transitionContext)
+            }
+        }
+    }
+    
+    func animatePresentation(using transitionContext: UIViewControllerContextTransitioning) {
+        guard
+            let toVC = transitionContext.viewController(forKey: .to)
+        else {
+            transitionContext.completeTransition(false)
+            return
+        }
+        
+        // Sets final frame of toVC
+        toVC.view.frame = transitionContext.finalFrame(for: toVC)
+        
+        // Transparent + increased scale for animation beginning
+        toVC.view.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        toVC.view.alpha = 0.0
+        
+        UIView.animate(withDuration: self.transitionDuration(using: transitionContext), delay: 0, options: .curveEaseOut, animations: { () -> Void in
+            toVC.view.alpha = 1.0
+            toVC.view.transform = CGAffineTransform.identity
+        }, completion: { (completed) -> Void in
+            transitionContext.completeTransition(true)
+        })
+    }
+    
+    func animateCancel(using transitionContext: UIViewControllerContextTransitioning) {
+        guard
+            let fromVC = transitionContext.viewController(forKey: .from)
+            else {
+                transitionContext.completeTransition(false)
+                return
+        }
+        
+        UIView.animate(withDuration: self.transitionDuration(using: transitionContext), delay: 0, options: .curveEaseIn, animations: { () -> Void in
+            fromVC.view.alpha = 0.0
+            fromVC.view.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }, completion: { (completed) -> Void in
+            transitionContext.completeTransition(true)
+        })
+    }
+    
+    func animateRequestSent(using transitionContext: UIViewControllerContextTransitioning) {
+        guard
+            let fromVC = transitionContext.viewController(forKey: .from)
+        else {
+            transitionContext.completeTransition(false)
+            return
+        }
+        
+        let translationY = fromVC.view.frame.origin.y + fromVC.view.frame.size.height
+        
+        UIView.animate(withDuration: self.transitionDuration(using: transitionContext), delay: 0, options: .curveEaseIn, animations: { () -> Void in
+            fromVC.view.transform = CGAffineTransform(translationX: 0, y: -translationY)
+        }, completion: { (completed) -> Void in
+            transitionContext.completeTransition(true)
+        })
+    }
+    
 }
